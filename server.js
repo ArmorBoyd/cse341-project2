d38fe331a404149d02ce4e224f4f2fee5859d72d
ArmorBoyd <boydpoblete@gmail.com>
2025-06-03 00:56:44 +0800

    update

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongodb = require('./connect');
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); 
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Swagger docs route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes'));
 
