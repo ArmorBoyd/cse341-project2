8a51e94c11236069694734d127e030bbc270977d
ArmorBoyd <boydpoblete@gmail.com>
2025-06-10 23:32:34 +0800

    update

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,5 @@
 const isAuthenticated = (req, res, next) => {
-    if (!req.session.user) {
+    if (req.session.user === undefined) {
         return res.status(401).json({ message: 'You do not have access' });
     }
     next();
