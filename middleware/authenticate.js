fa3d903bdac9f02f9b5ad2672e800ecf0ccc2422
ArmorBoyd <boydpoblete@gmail.com>
2025-06-10 23:27:52 +0800

    authenticated typo

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,7 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefinced) {
-        return res.status(401).json( 'You do not have access' );
+    if (!req.session.user) {
+        return res.status(401).json({ message: 'You do not have access' });
     }
     next();
 };
-
-module.exports = {
-    isAuthenticated
-}
+module.exports = { isAuthenticated };
