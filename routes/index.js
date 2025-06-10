77f7ab50293acdea53d0ab09e2b80bdba9c5b21c
ArmorBoyd <boydpoblete@gmail.com>
2025-06-10 21:04:17 +0800

    router.js in routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ router.use('/songs', require('./songs'));
 
 router.use('/anime', require('./anime'));
 
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if(err) {return next (err); }
+        res.redirect('/');
+    });    
+});
+
+
 router.get('/', (req, res) => {
     //#swagger.tags=['Hello World']
     res.send('Hello World');
