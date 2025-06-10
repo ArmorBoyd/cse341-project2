8423242f394e08559d59827e5b61f66387bc10d8
ArmorBoyd <boydpoblete@gmail.com>
2025-06-10 20:55:46 +0800

    installed cors and middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const session = require('express-session');
 const GithubStrategy = require('passport-github2').Strategy;
 const swaggerDocument = require('./swagger.json'); 
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,6 +28,32 @@ app.use((req, res, next) => {
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes'));
+
+passport.use(new GithubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+},
+function (accessToken, refreshToken, profile, done) {
+    // Here you would typically save the user to your database
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged out')});
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs'. session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
 
 mongodb.initDb((err) => {
     if (err) {
